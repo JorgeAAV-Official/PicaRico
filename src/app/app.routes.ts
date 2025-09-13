520c7b872a2fa890f47da4ca5d2a219810f3f7ed
JorgeAAV <ariasvargasjorgealejandro@gmail.com>
2025-09-13 16:20:08 -0500

    segundo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Principal } from './principal/principal';
 import { Contacto } from './contacto/contacto';
+import { Productos } from './productos/productos';
 
 export const routes: Routes = [
     { path: '', component: Principal }, 
-    { path: 'contacto', component: Contacto}
+    { path: 'contacto', component: Contacto},
+    { path: 'productos', component: Productos},
 ];
