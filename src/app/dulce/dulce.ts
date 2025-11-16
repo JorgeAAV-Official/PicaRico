5c8f6e28ba1c2602e9284c77f941f36c621ba8c7
Juan David <juandavidplata251@gmail.com>
2025-11-16 12:49:57 -0500

    Primer commit del proyecto Angular PicaRico

--- src/app/dulce/dulce.ts
+++ src/app/dulce/dulce.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CartService, Product } from '../services/cart.service';
 
 @Component({
   selector: 'app-dulce',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './dulce.html',
-  styleUrl: './dulce.css'
+  styleUrls: ['./dulce.css']
 })
 export class Dulce {
+  showToast = false;
+  showLoginError = false;
 
+  categoriaSeleccionada: string = 'todas';
+
+  constructor(public cartService: CartService) {}
+
+  async addToCart(name: string, price: number, description: string, image: string) {
+
+    if (!this.cartService.isUserLogged()) {
+      this.showLoginError = true;
+      setTimeout(() => this.showLoginError = false, 2000);
+      return;
+    }
+
+    const product: Product = { name, price, description, image, quantity: 1 };
+    await this.cartService.addToCart(product);
+
+    const cartIcon = document.querySelector('.cart-icon') as HTMLElement;
+    cartIcon?.classList.add('bounce');
+    setTimeout(() => cartIcon?.classList.remove('bounce'), 500);
+
+    this.showToast = true;
+    setTimeout(() => this.showToast = false, 1500);
+  }
+
+  filterCategory(category: string) {
+    this.categoriaSeleccionada = category;
+    console.log(`Categoría seleccionada: ${category}`);
+  }
 }
