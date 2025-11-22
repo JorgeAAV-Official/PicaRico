12816cd9f7d5d89b216d504d4498c2b2faa9889d
Juan David <juandavidplata251@gmail.com>
2025-11-21 22:27:39 -0500

    Primer commit del proyecto Angular PicaRico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,24 +2,20 @@ import { Routes } from '@angular/router';
 import { Principal } from './principal/principal';
 import { Contacto } from './contacto/contacto';
 import { Productos } from './productos/productos';
-import { Personalizar } from './personalizar/personalizar';
 import { Carrito } from './carrito/carrito';
 import { Intenso } from './intenso/intenso';
 import { Bandejas } from './bandejas/bandejas';
 import { Dulce } from './dulce/dulce';
-import { Promociones } from './promociones/promociones';
 import { Registro } from './registro/registro';
 export const routes: Routes = [
   { path: '', component: Principal },
   { path: 'contacto', component: Contacto },
   { path: 'productos', component: Productos },
-  { path: 'personalizar', component: Personalizar},
   { path: 'carrito', component: Carrito},
   { path: 'intenso', component: Intenso},
   { path: 'bandeja', component: Bandejas},
   {path: 'dulce', component: Dulce},
   {path: 'principal', component: Principal},
-  {path: 'promociones', component: Promociones},
   {path: 'registro', component: Registro}
 
 
